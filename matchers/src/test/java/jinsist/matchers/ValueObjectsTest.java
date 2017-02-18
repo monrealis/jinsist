1169b50f28ed51703339fd4cdc539facd996027c
Vytenis Bivainis <monrealis@gmail.com>
2017-02-18 02:43:57 +0200

    Improve naming of tests

--- matchers/src/test/java/jinsist/matchers/ValueObjectsTest.java
+++ matchers/src/test/java/jinsist/matchers/ValueObjectsTest.java
@@ -12,34 +12,34 @@ import org.junit.Test;
 import java.util.List;
 
 public class ValueObjectsTest {
-    private Pair pair = new Pair(1, "First");
-    private EqualityTester<Pair> tester = o -> pair.toList().equals(o.toList());
+    private Pair valueObject = new Pair(1, "First");
+    private EqualityTester<Pair> tester = o -> valueObject.toList().equals(o.toList());
 
     @Test
-    public void instanceEqualsItself() {
-        assertTrue(isPairEqualsAnother(pair));
+    public void valueObjectEqualsItself() {
+        assertTrue(valueObjectsEqualsOther(valueObject));
     }
 
     @Test
-    public void equalInstancesAreEqual() {
-        Pair otherEqual = new Pair(pair.getFirst(), pair.getSecond());
-        assertTrue(isPairEqualsAnother(otherEqual));
+    public void valueObjectsWithEqualFieldsAreEqual() {
+        Pair otherWithEqualFields = new Pair(valueObject.getFirst(), valueObject.getSecond());
+        assertTrue(valueObjectsEqualsOther(otherWithEqualFields));
     }
 
     @Test
-    public void ifAnyComponentDiffersThenNotEqual() {
-        Pair typeDiffers = new Pair(pair.getFirst() + 1, pair.getSecond());
-        assertFalse(isPairEqualsAnother(typeDiffers));
+    public void valueObjectsWithDifferentFieldsAreNotEqual() {
+        Pair otherWithDifferentField = new Pair(valueObject.getFirst() + 1, valueObject.getSecond());
+        assertFalse(valueObjectsEqualsOther(otherWithDifferentField));
     }
 
     @Test
-    public void ifTypesDifferThenNotEqual() {
-        assertFalse(isPairEqualsAnother(null));
-        assertFalse(isPairEqualsAnother(new Object()));
+    public void valueObjectsOfDifferentTypesAreNotEqual() {
+        assertFalse(valueObjectsEqualsOther(null));
+        assertFalse(valueObjectsEqualsOther(new Object()));
     }
 
-    private boolean isPairEqualsAnother(Object other) {
-        return ValueObjects.equal(pair, other, tester);
+    private boolean valueObjectsEqualsOther(Object other) {
+        return ValueObjects.equal(valueObject, other, tester);
     }
 
     private static class Pair {
